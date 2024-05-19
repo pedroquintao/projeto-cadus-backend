c409df2ab6479344a5561cae88bc753c1369dec8
pedroquintao <pedro.h.quintao@hotmail.com>
2024-05-19 13:40:04 -0300

    Método put alterado para patch

--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -5,7 +5,7 @@ const routes = express.Router();
 
 routes.get("/users", UserController.listUsers);
 routes.post("/users", UserController.registerUser);
-routes.put("/users/:id", UserController.updateUser);
+routes.patch("/users/:id", UserController.updateUser);
 routes.delete("/users/:id", UserController.deleteUser);
 
 export default routes;
