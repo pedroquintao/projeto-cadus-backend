20bd5b29fa84e0f25cf5d3a68eaf560f45d072e7
pedroquintao <pedro.h.quintao@hotmail.com>
2024-05-18 22:25:17 -0300

    Controladores e rotas para deletar e atualizar usuário foram implementadas

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -19,6 +19,27 @@ class UserController {
             res.status(500).json({message: `${erro.message} - falha ao cadastrar usuário`})
         }
     };
+    
+    static async updateUser (req, res) {
+        try {
+            const id = req.params.id;
+            await user.findByIdAndUpdate(id, req.body);
+            res.status(200).json({message: "Usuário atualizado com sucesso!"});
+        } catch(e) {
+            res.status(500).json({message: `${e.message} - Falha ao atualizar usuário!`});
+        }
+    }
+
+    static async deleteUser (req, res) {
+        try {
+            const id = req.params.id;
+            await user.findByIdAndDelete(id);
+            res.status(200).json({message: "Usuário excluído com sucesso!"});
+        } catch (e) {
+            res.status(500).json({message: `${e.message} - falha ao excluir usuário!`})
+        }
+    }
 }
+
 
 export default UserController;
--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -5,5 +5,7 @@ const routes = express.Router();
 
 routes.get("/users", UserController.listUsers);
 routes.post("/users", UserController.registerUser);
+routes.put("/users/:id", UserController.updateUser);
+routes.delete("/users/:id", UserController.deleteUser);
 
 export default routes;
